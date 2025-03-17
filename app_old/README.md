e8637c56a615ac79f54ed1eb11a009f8dc85af71
JFKakaJFK <j.koch@student.tudelft.nl>
2025-03-17 11:31:46 +0100

    removed old ui

--- app_old/README.md
+++
@@ -1,5 +0,0 @@
-# Mobile Controller
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/9c08a47b-1ae8-4dd4-869d-87ee8287e24e/deploy-status)](https://app.netlify.com/sites/mobile-controller/deploys)
-
-
--- app_old/src/components/button.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Button({ children, ...props }) {
-  return <button {...props} className={`${props.className || ''} text-dark-blue text-xs font-bold uppercase bg-baby-blue rounded-full py-3 px-10 hover:shadow transition-shadow`}>{children}</button>
-}
